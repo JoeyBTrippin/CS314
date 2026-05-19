49f84c881ccdcedb9804bfff2f5be2aac43a29eb
Joseph <josephwblecha@hotmail.com>
2026-05-19 15:14:20 -0500

    bug fixes in shell program. Add file child.c

--- class_notes/week2/shell.c
+++ class_notes/week2/shell.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
-#include <unistd.h> // nead for shel commands, ie. fork(), exec(), wait(),...
+#include <unistd.h> // nead for shel commands, ie. fork(), exec()
+#include <sys/wait.h>
 
 int main() {
 	int i = 123123;
 
-
 	int pid = fork();
 
 	if (pid != 0)
-		wait(pid);
+		waitpid(pid, NULL, 0);
 
 	if (pid == 0)
 		i=999999;
 
 	if (pid == 0)
-		execv("/user/bin/ls", NULL);
+		execv("./child", NULL);
 
-	printf("hello %d %d $p\n", pid, i, &i);
+	printf("hello %d %d %p\n", pid, i, &i);
 
 	return 0;
 }
