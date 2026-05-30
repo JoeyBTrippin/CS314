2ebf1d83654c9346a8901b265ff7ea0365416ecb
Joseph <josephwblecha@hotmail.com>
2026-05-30 12:31:23 -0500

    CREATE: project directory to hold all class projects. CREATE: project1 with starter code as defined in assignment handout. CREATE: hw2 with basic beginnings for both implementations, sem.c and pthread.c

---
+++ projects/mymalloc.h
@@ -0,0 +1,17 @@
+#ifndef MYMALLOC_H
+#define MYMALLOC_H
+
+#include <stddef.h>
+
+#define NUM_FASTBINS 4
+#define NUM_REGULAR_BINS 4
+
+void* my_malloc(size_t size) {
+
+
+}
+
+void my_free(void *ptr) {
+
+
+}
